1b047135177e086d549796ac50cc03b0399ae4ae
dev-g1 <irokagolden@gmail.com>
2023-10-13 18:11:36 +0100

    started working on the Header and Nav components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
 import './App.css';
+import Header from "./Components/Header";
 
 function App() {
-
+    return (
+        <>
+            <Header />
+            {/* <Main />
+            <Footer /> */}
+        </>
+    );
 }
 
 export default App;
